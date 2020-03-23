0c80df82e91a4683de9a5315c64fb72ddd30d765
Isis Khayaleeva <45292943+easydush@users.noreply.github.com>
2020-03-23 23:12:47 +0300

    Update Config.py

--- server/Config.py
+++ server/Config.py
@@ -1,5 +1,4 @@
 token = "869886648:AAESqRFFJIqI2qfuTNWG7rgdSeTO9tymDwk"
 connection_type = 'https'
-proxy = 'socks5://corap:ohmypass@a.li2sites.ru:80'
 tesseract_path = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 images_path = 'C:/Users/Isis/PycharmProjects/SoDa/server/files/tmp/'
