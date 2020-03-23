e074eeff744f8c631f5a146963f96b3691784dac
Isis Khayaleeva <45292943+easydush@users.noreply.github.com>
2020-03-23 23:14:03 +0300

    Delete Config.py

--- server/Config.py
+++
@@ -1,4 +0,0 @@
-token = "869886648:AAESqRFFJIqI2qfuTNWG7rgdSeTO9tymDwk"
-connection_type = 'https'
-tesseract_path = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
-images_path = 'C:/Users/Isis/PycharmProjects/SoDa/server/files/tmp/'
